a85dc6d4ffa65beee31faa378de81dbae87aa4b2
vijaykumar2552 <puttavijaykumar2552@gmail.com>
2026-09-18 16:04:16 +0530

    Complete TankSync project setup

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Tank_sync/',
-  optimizeDeps: {
-    exclude: [],
-  },
+  base: './',
 });
